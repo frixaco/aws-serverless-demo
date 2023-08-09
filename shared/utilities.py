e92dca1b68fe9444d2d4ab960c5d63d1250c86e3
frixaco <rustam21ashurmatov@gmail.com>
2023-08-09 18:27:21 +0500

    Test in-layer imports

--- shared/utilities.py
+++ shared/utilities.py
@@ -1,5 +1,8 @@
 import os
 import json
+import subprocess
+
+from database import db_conn
 
 def printer(text):
     print("Your text", text)
@@ -14,3 +17,10 @@ def check_ffmpeg_binary():
     print("ffmpeg binary found")
     return True
     
+
+def printer(text):
+    ffmpeg_path = "ffmpeg"
+    cmd = f"{ffmpeg_path} --version"
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    print(text, result, db_conn())
