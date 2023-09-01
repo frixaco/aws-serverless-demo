338b453638e6b66be9ed55202d844ded70296c4a
frixaco <rustam21ashurmatov@gmail.com>
2023-09-01 16:36:45 +0500

    Switch to Merged AppSync API (single CFN template)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf .aws-sam
-node schema-merger/index.js
-sam build --template template-root.yaml --profile admin@rr-ashurmatov-root
-sam package --force-upload --output-template-file packaged-template-root.yaml --profile admin@rr-ashurmatov-root
-sam deploy --template packaged-template-root.yaml --stack-name demo-1 --profile admin@rr-ashurmatov-root --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+
+sam build --template ./templates/root.yaml --profile admin@rr-ashurmatov-root
+sam package --force-upload --output-template-file ./templates/packaged-root.yaml --profile admin@rr-ashurmatov-root
+sam deploy --template ./templates/packaged-root.yaml --stack-name demo-1 --profile admin@rr-ashurmatov-root --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
